6e7019be19a8b1732dc35efdf5b78f81d621cf0b
AndrewRahn <AndrewRahn@users.noreply.github.com>
2016-05-02 13:26:48 -0600

    Updated exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -37,7 +37,7 @@ featureLayer.on('ready', function(){
 featureLayer.on('ready', function(){
   this.eachLayer(function(layer){
     layer.bindPopup('Welcome to ' + layer.feature.properties.name);
-                    })
+  })
   })
   
                 
